5d9432b0af28be80e06d1e1d5e46dbaf20992044
shuban-789 <cybergeek11929@gmail.com>
2025-06-20 17:11:30 -0700

    Revert "stuff ig"
    This reverts commit f25ec452cc1a7d53209ee0689ebb868da0fd5a15.

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/home/shuban/Code/misc/learn-physics-enm.github.io/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/home/shuban/Code/misc/learn-physics-enm.github.io/node_modules/infima/dist/css/default/default.css"),
-  require("/home/shuban/Code/misc/learn-physics-enm.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/shuban/Code/misc/learn-physics-enm.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/shuban/Code/misc/learn-physics-enm.github.io/src/css/custom.css"),
-];
